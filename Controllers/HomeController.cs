838739b01adbb0859c3f2a2a027ee9ebe4cadc6b
pramodmanure <146764220+pramodmanure@users.noreply.github.com>
2023-10-03 20:28:12 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using MarketingCodingAssignment.Models;
+using MarketingCodingAssignment.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MarketingCodingAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SearchEngine _searchEngine;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _searchEngine = new SearchEngine();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public JsonResult Search(string searchString, int start, int rows, int? durationMinimum, int? durationMaximum, double? voteAverageMinimum)
+        {
+            SearchResultsViewModel searchResults = _searchEngine.Search(searchString, start, rows, durationMinimum, durationMaximum, voteAverageMinimum);
+            return Json(new {searchResults});
+        }
+
+        [HttpPost]
+        public void ReloadIndex()
+        {
+            DeleteIndex();
+            PopulateIndex();
+            return;
+        }
+
+        // Delete the contents of the lucene index 
+        public void DeleteIndex()
+        {
+            _searchEngine.DeleteIndex();
+            return;
+        }
+
+        // Read the data from the csv and feed it into the lucene index
+        public void PopulateIndex()
+        {
+            _searchEngine.PopulateIndexFromCsv();
+            return;
+        }
+
+    }
+}
+
---
+++ Models/FilmLuceneRecord.cs
@@ -0,0 +1,52 @@
+﻿namespace MarketingCodingAssignment.Models
+{
+    public class FilmLuceneRecord
+    {
+        public string? Id
+        {
+            get; set;
+        }
+
+        public float? Score
+        {
+            get; set;
+        }
+
+        public string? Title
+        {
+            get; set;
+        }
+
+        public string? Overview
+        {
+            get; set;
+        }
+
+        public int Runtime
+        {
+            get; set;
+        }
+
+        public string? Tagline
+        {
+            get; set;
+        }
+
+        public long? Revenue
+        {
+            get; set;
+        }
+
+        public DateTime? ReleaseDate
+        {
+            get; set;
+        }
+
+        public double? VoteAverage
+        {
+            get; set;
+        }
+
+
+    }
+}
